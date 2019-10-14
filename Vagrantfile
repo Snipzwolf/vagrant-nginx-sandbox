71e257554e170a49a047a1acf343e2931c27edcf
John Copsey <Snipzwolf@users.noreply.github.com>
2019-10-14 18:53:09 +0100

    initial setup

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-18.04"
+
+  config.ssh.forward_agent = true
+
+  config.vm.network "forwarded_port", guest: 22, host: 2028
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "vagrant-ide-nginx"
+    vb.cpus = 4
+    vb.memory = "1024"
+    vb.linked_clone = true
+
+    vb.customize ["modifyvm", :id, "--ioapic", "on"]
+    vb.customize ["storagectl", :id, "--name", "IDE Controller", "--remove" ]
+  end
+
+  config.vm.provision "file", source: "./ansible", destination: "/home/vagrant/ansible"
+  config.vm.provision "file", source: "./.private", destination: "/home/vagrant/.private"
+
+  config.vm.provision :shell, :path => "provision.sh"
+end
---
+++ provision.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+export DEBCONF_NONINTERACTIVE_SEEN="true";
+export DEBIAN_FRONTEND="noninteractive";
+
+update-alternatives --set editor /bin/nano;
+touch /etc/profile.d/editor.sh;
+echo "export EDITOR=nano" >> /etc/profile.d/editor.sh;
+
+apt-get update;
+apt-get install -qqy linux-headers-generic software-properties-common htop mtr tmux wget ca-certificates p7zip-full python3-pip git;
+
+pip3 install -q -I ansible==2.8.1;
+pip3 install -q -I awscli boto boto3 botocore dnspython netaddr;
+
+add-apt-repository -y ppa:webupd8team/atom;
+apt-get update -qq;
+apt-get install -qqy atom;
+
+echo -e "\
+[defaults]\n\
+inventory = /home/vagrant/ansible/hosts\n\
+vault-password-file = /home/vagrant/ansible/ansible_vault\n\
+roles_path = /home/vagrant/ansible/roles" > /home/vagrant/ansible/ansible.cfg;
+
+echo -e "[nginx]\nlocalhost" > /home/vagrant/ansible/hosts;
+
+set -x;
+
+chown -R vagrant: /home/vagrant/ansible;
+cd /home/vagrant/ansible;
+
+ansible-playbook -c local projects/nginx-server.yml;
